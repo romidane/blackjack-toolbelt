838848ac090f601a2c4ac4261968a73b7f6fa051
Callum Barratt <callum.barratt@sky.uk>
2015-11-16 12:20:15 +0000

    Fixed fixture generator

--- createFixture.js
+++ createFixture.js
@@ -11,11 +11,11 @@ var Handlebars = require('handlebars');
 var brand = 'base';
 
 const createFixture = () => {
-    const template = fs.readFileSync(fileSelector(brand, 'template'));
-    const data = fs.readFileSync(fileSelector(brand, 'data'));
-    const content = Handlebars.compile(contents)(data);
+    const template = fs.readFileSync(fileSelector(brand, 'template'), 'utf8');
+    const data = fs.readFileSync(fileSelector(brand, 'data'), 'utf8');
+    const content = Handlebars.compile(template)(data);
 
-    fs.writeFileSync(`test/fixtures/${brand}`, content);
+    fs.writeFileSync(`test/fixtures/${brand}.html`, content);
 }
 
 createFixture();
