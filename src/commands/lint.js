6dd4a0d41244aea908398c2fe796ee5df43d8537
Josh Nesbitt <josh@wearestac.com>
2016-04-20 16:14:52 +0100

    Implement lint command.

--- src/commands/lint.js
+++ src/commands/lint.js
@@ -1,3 +1,6 @@
-export function call(){
-  console.log("[Lint] Call()");
+import exec from '../util/exec';
+import { ESLINT_BIN, ESLINT_CONF } from '../constants';
+
+ export function call(){
+  exec(`${ESLINT_BIN} -c ${ESLINT_CONF} lib src test`);
 };
