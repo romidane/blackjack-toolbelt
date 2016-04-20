59d78fff5b5f082d959a34044e40a530e17a945f
Josh Nesbitt <josh@wearestac.com>
2016-04-20 16:24:30 +0100

    Fix listing errors in application.js.

--- src/application.js
+++ src/application.js
@@ -1,26 +1,41 @@
 import commander from 'commander';
 import info from '../package.json';
 
-export function run(){
-
+export function run() {
   commander.version(info.version);
 
-  function executeCommand(options){
+  function executeCommand(options) {
     const name = options._name;
     const command = require(`./commands/${name}`);
 
     console.log(`Running '${name}' command...\n`);
 
     command.call(options);
-  };
+  }
 
-  commander.command('bundle').description('Creates a production bundle of CSS/JS.').action(executeCommand);
-  commander.command('init').description('Create a new component.').action(executeCommand);
-  commander.command('lint').description('Lint all in test and lib dirs.').action(executeCommand);
-  commander.command('mocha').description('Run JS mocha in your test directory.').action(executeCommand);
-  commander.command('preview').description('Preview the built component locally.').action(executeCommand);
-  commander.command('test').description('Run JS tests in your test directory.').action(executeCommand);
+  commander.command('bundle').
+    description('Creates a production bundle of CSS/JS.').
+    action(executeCommand);
 
-  commander.parse(process.argv)
+  commander.command('init').
+    description('Create a new component.').
+    action(executeCommand);
 
+  commander.command('lint').
+    description('Lint all in test and lib dirs.').
+    action(executeCommand);
+
+  commander.command('mocha').
+    description('Run JS mocha in your test directory.').
+    action(executeCommand);
+
+  commander.command('preview').
+    description('Preview the built component locally.').
+    action(executeCommand);
+
+  commander.command('test').
+    description('Run JS tests in your test directory.').
+    action(executeCommand);
+
+  commander.parse(process.argv);
 }
