4447f4f6de4d2f93888c3d177fd2aa3a0e965d4a
Josh Nesbitt <josh@wearestac.com>
2016-04-28 15:18:50 +0100

    Ensure package.json has required dependencies.

--- src/project-creation/package-creator.js
+++ src/project-creation/package-creator.js
@@ -16,10 +16,21 @@ export default {
   "license": "BSD-3-Clause",
   "dependencies": {
     "babel-loader": "^6.2.4",
+    "babel-core": "^6.7.4",
+    "babel-loader": "^6.2.4",
+    "babel-polyfill": "^6.7.4",
+    "babel-preset-es2015": "^6.6.0",
+    "babel-preset-react": "^6.5.0",
+    "babel-preset-stage-0": "^6.5.0",
+    "babel-register": "^6.7.2",
+    "css-loader": "^0.23.1",
+    "node-sass": "^3.6.0",
     "react": "^0.14.8",
     "react-dom": "^15.0.1",
+    "sass-loader": "^3.2.0",
     "sky-toolkit": "github:sky-uk/toolkit",
-    "sky-blackjack-tools": "github:sky-uk/blackjack-tools"
+    "sky-blackjack-tools": "github:sky-uk/blackjack-tools",
+    "style-loader": "^0.13.1"
   },
   "blackjack": {
     "preview": "src/preview.js"
