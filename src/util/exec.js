05b2f12b21634d8356ba49234c215692d318edd8
Josh Nesbitt <josh@wearestac.com>
2016-05-09 11:26:32 +0100

    Increase maxBuffer for processes.

--- src/util/exec.js
+++ src/util/exec.js
@@ -2,7 +2,7 @@ import { exec } from 'child_process';
 import logger from './logger';
 
 export default function (command) {
-  exec(`${command} --color`, (error, stdout, stderr) => {
+  exec(`${command} --color`, { maxBuffer: 1024 * 1000 }, (error, stdout, stderr) => {
     logger.info(`${stdout}`);
 
     if (stderr) {
