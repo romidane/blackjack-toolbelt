39d0bfa391cc12623c5fac3a86021300ad1246f1
Josh Nesbitt <josh@wearestac.com>
2016-05-09 16:52:09 +0100

    Revert patch.

--- src/constants.js
+++ src/constants.js
@@ -1,21 +1,6 @@
 const BLACKJACK_DIR = require('path').dirname(__dirname);
+const MODULES_DIR = `${BLACKJACK_DIR}/node_modules`;
 const WORKING_DIR = process.cwd();
-
-let MODULES_DIR = `${BLACKJACK_DIR}/node_modules`;
-
-const STANDALONE_INSTALL = process.env.npm_package_dependencies_blackjack_toolbelt === undefined;
-
-// This is a work around to stop NPM trying to be clever.
-// If we don't implement this fix, NPM will resolve node_modules
-// incorrectly for nested package installs. This means that we
-// have to sniff whether the package is installed on it's own
-// via NPM, or as part of a bundle in another project. Once we
-// know this, we can ensure we point to the correct modules dir
-// (as NPM now flattens the node_modules directory structure).
-
-if (!STANDALONE_INSTALL){
-  MODULES_DIR = `${BLACKJACK_DIR}/..`;
-}
 
 module.exports = {
   WORKING_DIR,
